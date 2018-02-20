ef144e3d738e168f6c7fdb5d0c63219ce463f86f
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-19 22:13:52 -0500

    completed edit component

--- src/app/client/edit/edit.component.ts
+++ src/app/client/edit/edit.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService, Blog } from '../../services/data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscriptions: Subscription;
 
-  ngOnInit() {
+  blog_id: Number = null;
+  edit_blog: Blog = { title: '', description: '' };
+  blogs: Array<Blog> = [];
+  error: Boolean = false;
+
+  constructor(private route: ActivatedRoute, private dataService: DataService, private router: Router) {
+    this.subscriptions = new Subscription();
   }
 
+  ngOnInit() {
+    this.blog_id = parseInt(this.route.snapshot.paramMap.get('id'));
+    console.log('Navigated to Blog ID ', this.blog_id);
+
+    this.dataService.getBlogs();
+
+    const blogsSubscription = this.dataService.blogs$.subscribe((blogs: Array<Blog>) => {
+      console.log('Blogs Retrieved: ', blogs);
+      this.blogs = blogs;
+      this.error = false;
+
+      this.edit_blog = this.blogs.filter((item, index) => this.blog_id === index)[0];
+    });
+
+    const blogsErrorSubscription = this.dataService.blogsError$.subscribe((error: Response) => {
+      console.log('Error Retrieving Blogs: ', JSON.stringify(error));
+      this.error = true;
+    });
+
+    this.subscriptions.add(blogsSubscription);
+    this.subscriptions.add(blogsErrorSubscription);
+  }
+
+  submit(form) {
+    console.log(form);
+    this.router.navigate(['/list']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
